842f0b1555b41dad99b4caa169b905843f069c95
Tomás Zambujo <a89797@ualg.pt>
2025-11-25 15:53:51 +0000

    altereio titutlo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'ADORO ISTO',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
